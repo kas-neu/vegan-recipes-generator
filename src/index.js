1b7f0276cc196b288965dcc737bae870b9ff8ee1
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-01-03 11:29:00 +0000

    added basic js form handling, added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: `<div class="recipe-title">Recipe title</div>
+     <div class="ingredient-list">
+           <ul>
+          <li>ingredient one</li>
+          <li>ingredient two</li>
+         <li>ingredient three</li>
+         <li>ingredient four</li>
+     </ul>
+ </div>
+   <div class="cooking-instructions">Cooking instructions</div>
+  <div>Enjoy! 😋</div>`,
+    autoStart: true,
+    delay: 10,
+    cursore: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
